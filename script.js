e6b9c299f3b4e60c8c9996ca34666f52917fbcb2
Harsh Sharma <158835705+h4rSheys@users.noreply.github.com>
2024-03-02 19:24:52 +0530

    Added styling

---
+++ script.js
@@ -0,0 +1,12 @@
+function updateClock() {
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
+
+    const clockElement = document.getElementById('clock');
+    clockElement.textContent = `${hours}:${minutes}:${seconds}`;
+  }
+
+  setInterval(updateClock, 1000);
+  updateClock();
